1adac9de119eb5f56202b423045e716c41ff9271
thackerdynasty <70179471+thackerdynasty@users.noreply.github.com>
2025-07-03 22:47:09 -0500

    add pipe renderer

--- src/main/java/com/dhyanthacker/betterautomation/BetterAutomationClient.java
+++ src/main/java/com/dhyanthacker/betterautomation/BetterAutomationClient.java
@@ -1,15 +1,20 @@
 package com.dhyanthacker.betterautomation;
 
+import com.dhyanthacker.betterautomation.block.entity.ModBlockEntities;
+import com.dhyanthacker.betterautomation.block.entity.renderer.PipeBlockEntityRenderer;
 import com.dhyanthacker.betterautomation.screen.ModScreenHandlers;
 import com.dhyanthacker.betterautomation.screen.custom.screen.ElectricFurnaceScreen;
 import com.dhyanthacker.betterautomation.screen.custom.screen.SolarPanelScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class BetterAutomationClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HandledScreens.register(ModScreenHandlers.SOLAR_PANEL_SCREEN_HANDLER, SolarPanelScreen::new);
         HandledScreens.register(ModScreenHandlers.ELECTRIC_FURNACE_SCREEN_HANDLER, ElectricFurnaceScreen::new);
+
+        BlockEntityRendererFactories.register(ModBlockEntities.PIPE_BE, PipeBlockEntityRenderer::new);
     }
 }
---
+++ src/main/java/com/dhyanthacker/betterautomation/block/entity/renderer/PipeBlockEntityRenderer.java
@@ -0,0 +1,44 @@
+package com.dhyanthacker.betterautomation.block.entity.renderer;
+
+import com.dhyanthacker.betterautomation.BetterAutomation;
+import com.dhyanthacker.betterautomation.block.entity.custom.PipeBlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.LightType;
+import net.minecraft.world.World;
+
+public class PipeBlockEntityRenderer implements BlockEntityRenderer<PipeBlockEntity> {
+    public PipeBlockEntityRenderer(BlockEntityRendererFactory.Context context) {}
+
+    @Override
+    public void render(PipeBlockEntity entity, float tickDelta, MatrixStack matrices,
+                       VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        ItemRenderer itemRenderer = MinecraftClient.getInstance().getItemRenderer();
+        ItemStack stack = entity.getStack(0);
+        if (stack.isEmpty()) return; // Don't render if the stack is empty
+        BetterAutomation.LOGGER.info("Rendering ItemStack: " + stack.getName().getString());
+
+        matrices.push();
+        matrices.translate(.5f, 1.15f, .5f);
+        matrices.scale(.5f, .5f, .5f);
+
+        itemRenderer.renderItem(stack, ModelTransformationMode.GUI, getLightLevel(entity.getWorld(),
+                entity.getPos()), OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, entity.getWorld(), 1);
+        matrices.pop();
+    }
+
+    private int getLightLevel(World world, BlockPos pos) {
+        int bLight = world.getLightLevel(LightType.BLOCK, pos);
+        int sLight = world.getLightLevel(LightType.SKY, pos);
+        return LightmapTextureManager.pack(bLight, sLight);
+    }
+}
